06a1d36544bd3c3b1ddedfa88d56628a64f19691
ilyasdabholkar <ilyasdabholkar9@gmail.com>
2025-02-13 11:43:32 +0530

    using react query

---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+import "bootstrap/dist/css/bootstrap.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions : {
+    queries : {
+      retry : 3,
+      staleTime : 10 * 1000 //10 sec
+    }
+  }
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
---
+++ src/react-query/hooks/usePosts.ts
@@ -0,0 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+
+const usePosts = (userId : number | undefined) => {
+  const fetchPosts = () =>
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts",{
+        params : {
+            userId
+        }
+      })
+      .then((res) => res.data);
+
+  return useQuery<Post[], Error>({
+    //users/1/posts
+    queryKey: userId ? ["users",userId,"posts"] : ["posts"],
+    queryFn: fetchPosts,
+  });
+};
+
+export default usePosts;
