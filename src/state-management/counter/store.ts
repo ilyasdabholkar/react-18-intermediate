368ba0704be3e72ce346a0d0f889dc69c8027513
ilyasdabholkar <ilyasdabholkar9@gmail.com>
2025-02-14 13:24:16 +0530

    Adding devtools to inspect zustand store

--- src/state-management/counter/store.ts
+++ src/state-management/counter/store.ts
@@ -1,3 +1,4 @@
+import { mountStoreDevtool } from "simple-zustand-devtools";
 import { create } from "zustand";
 
 interface CounterStore{
@@ -11,5 +12,9 @@ const useCounterStore = create<CounterStore>(set => ({
     increment : () => set(store => ({counter : store.counter + 1})),
     reset : () => set(store => ({counter : 0}))
 }));
+
+if(process.env.NODE_ENV == 'development'){
+    mountStoreDevtool('Counter Store',useCounterStore)
+}
 
 export default useCounterStore;
